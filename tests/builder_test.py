fc3aaa4c71658bcd0a51f18b87490f2c0060fc46
luyuh <774964107@qq.com>
2024-04-02 11:29:10 +0800

    change project directory

--- tests/builder_test.py
+++ tests/builder_test.py
@@ -1,9 +1,4 @@
 import unittest
-import sys, os
-from os import path
-top = path.abspath(path.join(path.dirname(__file__), '../../'))
-sys.path.append(top)
-
 from sqlbuilder.grammar import Grammar, Builder
 
 
@@ -13,6 +8,5 @@ class BuilderTest(unittest.TestCase):
         self.builder = Builder(None, Grammar('tb_'))
 
     def test_hello(self):
-        # print(sys.path)
         sql = self.builder.select('id', 'name').table('user').where('id', 3).to_sql()
         print(sql)
